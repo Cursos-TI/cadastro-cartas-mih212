8c2040e55dc442a564787e4b19d1575b79af29d8
mih212 <milaalves981@gmail.com>
2026-05-01 11:46:04 +0000

    fiz o desafio nivel  aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,9 @@ int main()
     float area;
     float PIB;
     int pontos_turisticos;
+    float desidade_populacional;
+    float pib_per_capita;
+
   
     char estado2;
     char codigo_da_carta2[3];
@@ -18,7 +21,9 @@ int main()
     float area2;
     float PIB2;
     int pontos_turisticos2;
-    
+    float desidade_populacional2;
+    float pib_per_capita2;
+
 
     printf("      Carta1: \n\n");
     printf("Digite seu estado: ");
@@ -26,11 +31,11 @@ int main()
     // printf("O estado é: %c\n", estado);
 
     printf("Digite o código da carta: ");
-    scanf("%s", &codigo_da_carta);
+    scanf("%s", codigo_da_carta);
     // printf("O codigo da carta é: %s\n", codigo_da_carta);
  
     printf("Digite o nome da cidade: ");
-    scanf("%s", &nome_da_cidade);
+    scanf("%s", nome_da_cidade);
     // printf("O nome da cidade é: %s\n", nome_da_cidade);
 
     printf("Digite o número da população: ");
@@ -55,11 +60,11 @@ int main()
     // printf("O estado é: %c\n", estado);
 
     printf("Digite o código da carta: ");
-    scanf("%s", &codigo_da_carta2);
+    scanf("%s", codigo_da_carta2);
     // printf("O codigo da carta é: %s\n", codigo_da_carta);
 
     printf("Digite o nome da cidade: ");
-    scanf("%s", &nome_da_cidade2);
+    scanf("%s", nome_da_cidade2);
     // printf("O nome da cidade é: %s\n", nome_da_cidade);
 
     printf("Digite o número da população: ");
@@ -78,7 +83,12 @@ int main()
     scanf("%d", &pontos_turisticos2);
     //    printf("Os de pontos turisticos é: %d\n", pontos_turisticos);
 
-    
+     desidade_populacional = numero_da_populacao / area;
+    pib_per_capita = PIB / numero_da_populacao;
+
+    desidade_populacional2 = numero_da_populacao2 / area2;
+    pib_per_capita2 = PIB2 / numero_da_populacao2;
+
     printf("\n\nCarta 1:\n");
     printf("O estado é: %c\n", estado);
     printf("O codigo da carta é: %s\n", codigo_da_carta);
@@ -87,7 +97,8 @@ int main()
     printf("A area é: %f\n", area);
     printf("O PIB é: %f\n", PIB);
     printf("Os de pontos turisticos é: %d\n", pontos_turisticos);
-    
+    printf("A densidade populacional é: %f\n", desidade_populacional);
+    printf("O PIB per capita é: %f\n", pib_per_capita);
     printf("\n\nCarta 2:\n");
     printf("O estado é: %c\n", estado2);
     printf("O codigo da carta é: %s\n", codigo_da_carta2);
@@ -96,6 +107,8 @@ int main()
     printf("A area é: %f\n", area2);
     printf("O PIB é: %f\n", PIB2);
     printf("Os de pontos turisticos é: %d\n", pontos_turisticos2);
-    
+    printf("A densidade populacional é: %f\n", desidade_populacional2);
+    printf("O PIB per capita é: %f\n", pib_per_capita2);
+
     // return 0;
 }
